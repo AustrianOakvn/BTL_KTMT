b5a89a31f5c49c672b318bfc51475600cfd20f31
BHoang <something@gmail.com>
2021-07-12 17:55:36 +0700

    add my median

---
+++ Windows_platform/MyMedianFilter/MyMedianFilter/kernel.cu
@@ -0,0 +1,147 @@
+﻿//
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+//
+//#include <stdio.h>
+//
+//
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/imgproc.hpp>
+//#include <opencv2/imgcodecs.hpp>
+////#include <opencv2/cudaimgproc.hpp>
+//#include <iostream>
+//
+//__host__ __device__ void swap(int* a, int* b)
+//{
+//	int t = *a;
+//	*a = *b;
+//	*b = t;
+//}
+//__host__ __device__ int partition(int arr[], int low, int high)
+//{
+//	int pivot = arr[high];    // pivot
+//	int i = (low - 1);  // Index of smaller element
+//
+//	for (int j = low; j <= high - 1; j++)
+//	{
+//
+//		if (arr[j] <= pivot)
+//		{
+//			i++;    // increment index of smaller element
+//			swap(&arr[i], &arr[j]);
+//		}
+//	}
+//	swap(&arr[i + 1], &arr[high]);
+//	return (i + 1);
+//}
+//
+///* The main function that implements QuickSort
+// arr[] --> Array to be sorted,
+//  low  --> Starting index,
+//  high  --> Ending index */
+//__host__ __device__ void quickSort(int arr[], int low, int high)
+//{
+//	if (low < high)
+//	{
+//		/* pi is partitioning index, arr[p] is now
+//		   at right place */
+//		int pi = partition(arr, low, high);
+//
+//		// Separately sort elements before
+//		// partition and after partition
+//		quickSort(arr, low, pi - 1);
+//		quickSort(arr, pi + 1, high);
+//	}
+//}
+//
+//
+//__global__ void median_filter(int* input_image, int* output_image, int image_width, int image_height, int kernel_size) {
+//	int row = blockIdx.y * blockDim.y + threadIdx.y;
+//	int col = blockIdx.x * blockDim.x + threadIdx.x;
+//	//std::vector<int> kernel(kernel_size, 0);
+//	int* kernel = new int[kernel_size*kernel_size];
+//	
+//	if (row == 0 || col == 0 || row == image_height - 1 || col == image_width - 1) {
+//		output_image[row * image_width + col] = 0;
+//	}
+//	else {
+//		for (int i = 0; i < kernel_size; i++) {
+//			for (int j = 0; j < kernel_size; j++) {
+//				kernel[i * kernel_size + j] = input_image[(row + i - 1) * image_width + (col + j - 1)];
+//			}
+//		}
+//		//std::sort(kernel.begin(), kernel.end());
+//		//std::sort(kernel, kernel + kernel_size - 1);
+//		quickSort(kernel, 0, kernel_size);
+//		output_image[row * image_width + col] = kernel[4];
+//		/*std::cout << kernel[4];*/
+//		
+//	}
+//}
+//
+//void cvImgToArray(const cv::Mat& mat, int *arr, int row, int col) {
+//	/*size_t arr_size = row * col * sizeof(unsigned char);
+//	arr = new unsigned char[arr_size];*/
+//	for (int i = 0; i < row; i++) {
+//		for (int j = 0; j < col; j++) {
+//			arr[i * row + j] = int(mat.at<unsigned char>(i, j));
+//			
+//		}
+//	}
+//	std::cout << int(arr[0]);
+//	std::cout << int(mat.at<unsigned char>(0, 0));
+//	//std::cout << arr;
+//}
+//
+//
+//int main()
+//{
+//	/*Mat image = Mat::zeros(300, 600, CV_8UC3);
+//	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
+//	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+//	imshow("Display Window", image);
+//	waitKey(0);
+//	return 0;*/
+//	//std::string image_path = "D:\\pythonProject\\Salt_Peper_Noise\\lena_noisy512.bmp";
+//	std::string image_path = "C:\\Users\\AustrianOak\\Pictures\\test_2.bmp";
+//
+//	std::string output_path = "D:\\pythonProject\\Salt_Peper_Noise\\example.bmp";
+//	cv::Mat img = imread(image_path, cv::IMREAD_GRAYSCALE);
+//	if (img.empty()) {
+//		std::cout << "Cout not rad the image" << image_path << std::endl;
+//		return 1;
+//	}
+//	int img_size = img.rows * img.cols;
+//	int* img_arr = new int [img_size];
+//	int* img_arr_devices;
+//	int* output_img;
+//	int* output_img_host;
+//	cvImgToArray(img, img_arr, img.rows, img.cols);
+//	cudaMalloc(&img_arr_devices, img_size);
+//	cudaMalloc(&output_img, img_size);
+//	cudaMemcpy(img_arr_devices, img_arr, img_size, cudaMemcpyHostToDevice);
+//	output_img_host = new int[img_size];
+//	int threads = 16;
+//	int blocks = (img_size + threads - 1) / threads;
+//
+//	dim3 NUM_THREADS(threads, threads);
+//	dim3 NUM_BLOCKS(blocks, blocks);
+//	int kernel_size = 3;
+//	//std::cout << img_arr;
+//	median_filter << <NUM_BLOCKS, NUM_THREADS >> > (img_arr_devices, output_img, img.rows, img.cols, kernel_size);
+//	cudaDeviceSynchronize();
+//	cudaMemcpy(output_img_host, output_img, img_size, cudaMemcpyDeviceToHost);
+//	std::cout << int(output_img_host[0]);
+//	cv::Mat result(img.rows, img.cols, CV_8UC1, output_img_host);
+//	cv::imwrite(output_path, result);
+//	
+//	imshow("Display window", result);
+//	//std::cout << img << std::endl;
+//	int k = cv::waitKey(0);
+//	if (k == 's') {
+//		cv::imwrite("sth.jpg", img);
+//	}
+//	
+//	return 0;
+//}
---
+++ Windows_platform/MyMedianFilter/MyMedianFilter/median_filter.cu
@@ -0,0 +1,277 @@
+﻿#include <cstdlib>
+#include "cstdlib"
+#include <process.h>
+#include <malloc.h>
+#include <ctime>
+#include <string>
+#include <iostream>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "cuda.h"
+#include "cuda_runtime_api.h"
+#include "Bitmap.h"
+#include <device_functions.h>
+using namespace std;
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+#define MEDIAN_DIMENSION 3
+#define MEDIAN_LENGTH 9
+
+#define BLOCK_WIDTH 16
+#define BLOCK_HEIGHT 16
+
+inline void gpuAssert(cudaError_t code, char* file, int line, bool abort = true)
+{
+	if (code != cudaSuccess)
+	{
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
+	}
+}
+int iDivUp(int a, int b) { return ((a % b) != 0) ? (a / b + 1) : (a / b); }
+
+__global__ void medianFilter_gpu(unsigned char* Input_Image, unsigned char *Output_Image, int Image_Width, int Image_Height) {
+	const int tx_l = threadIdx.x;
+	const int ty_l = threadIdx.y;
+
+	const int tx_g = blockIdx.x * blockDim.y + tx_l;
+	const int ty_g = blockIdx.y * blockDim.y + ty_l;
+
+	__shared__ unsigned char smem[BLOCK_WIDTH + 2][BLOCK_HEIGHT + 2];
+
+	if (tx_l == 0) smem[tx_l][ty_l + 1] = 0;
+	else if (tx_l == BLOCK_WIDTH - 1) smem[tx_l + 2][ty_l + 1] = 0;
+	if (ty_l == 0) {
+		smem[tx_l + 1][ty_l] = 0;
+		if (tx_l == 0) smem[tx_l][tx_l] = 0;
+		else if (tx_l == BLOCK_WIDTH - 1) smem[tx_l + 2][ty_l] = 0;
+
+	}
+	else if (tx_l == BLOCK_HEIGHT - 1) {
+		smem[tx_l + 1][ty_l + 2] = 0;
+		if (tx_l == 0) smem[tx_l][ty_l + 2] = 0;
+		else if (tx_l == BLOCK_WIDTH - 1) smem[tx_l + 2][ty_l + 2] = 0;
+	}
+
+	if ((tx_l == 0) && ((tx_g > 0)))                                      
+		smem[tx_l][ty_l + 1] = Input_Image[ty_g * Image_Width + tx_g - 1];      // --- left border
+	else if ((tx_l == BLOCK_WIDTH - 1) && (tx_g < Image_Width - 1))         
+		smem[tx_l + 2][ty_l + 1] = Input_Image[ty_g * Image_Width + tx_g + 1];      // --- right border
+	if ((ty_l == 0) && (ty_g > 0)) {
+		smem[tx_l + 1][ty_l] = Input_Image[(ty_g - 1) * Image_Width + tx_g];    // --- upper border
+		if ((tx_l == 0) && ((tx_g > 0)))                                  
+			smem[tx_l][ty_l] = Input_Image[(ty_g - 1) * Image_Width + tx_g - 1];  // --- top-left corner
+		else if ((tx_l == BLOCK_WIDTH - 1) && (tx_g < Image_Width - 1))     
+			smem[tx_l + 2][ty_l] = Input_Image[(ty_g - 1) * Image_Width + tx_g + 1];  // --- top-right corner
+	}
+	else if ((ty_l == BLOCK_HEIGHT - 1) && (ty_g < Image_Height - 1)) {
+		smem[tx_l + 1][ty_l + 2] = Input_Image[(ty_g + 1) * Image_Width + tx_g];    // --- bottom border
+		if ((tx_l == 0) && ((tx_g > 0)))                                 
+			smem[tx_l][ty_l + 2] = Input_Image[(ty_g - 1) * Image_Width + tx_g - 1];  // --- bottom-left corder
+		else if ((tx_l == BLOCK_WIDTH - 1) && (tx_g < Image_Width - 1))     
+			smem[tx_l + 2][ty_l + 2] = Input_Image[(ty_g + 1) * Image_Width + tx_g + 1];  // --- bottom-right corner
+	}
+	__syncthreads();
+	unsigned char v[9] = { smem[tx_l][ty_l], smem[tx_l + 1][ty_l], smem[tx_l + 2][ty_l],
+						  smem[tx_l][ty_l + 1], smem[tx_l + 1][ty_l + 1], smem[tx_l + 2][ty_l + 1],
+						  smem[tx_l][ty_l + 2], smem[tx_l + 1][ty_l + 2], smem[tx_l + 2][ty_l + 2] };
+
+	// Buble sort
+	for (int i = 0; i < 6; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (v[i] = v[j]) {
+				unsigned char tmp = v[i];
+				v[i] = v[j];
+				v[j] = tmp;
+			}
+		}
+	}
+	Output_Image[ty_g * Image_Width + tx_g] = v[4];
+}
+
+//__global__ void Original_Kernel_Function(unsigned char* Input_Image, unsigned char* Output_Image, int Image_Width, int Image_Height) {
+//	__shared__ unsigned short surround[BLOCK_WIDTH * BLOCK_HEIGHT][9];
+//
+//	int iterator;
+//
+//	const int x = blockDim.x * blockIdx.x + threadIdx.x;
+//	const int y = blockDim.y * blockIdx.y + threadIdx.y;
+//	const int tid = threadIdx.y * blockDim.x + threadIdx.x;
+//
+//	if ((x >= (Image_Width - 1)) || (y >= Image_Height - 1) || (x == 0) || (y == 0)) return;
+//
+//	// --- Fill shared memory
+//	iterator = 0;
+//	for (int r = x - 1; r <= x + 1; r++) {
+//		for (int c = y - 1; c <= y + 1; c++) {
+//			surround[tid][iterator] = Input_Image[c * Image_Width + r];
+//			iterator++;
+//		}
+//	}
+//
+//	// --- Sort shared memory to find the median using Bubble Short
+//	for (int i = 0; i < 5; ++i) {
+//
+//		// --- Find the position of the minimum element
+//		int minval = i;
+//		for (int l = i + 1; l < 9; ++l) if (surround[tid][l] < surround[tid][minval]) minval = l;
+//
+//		// --- Put found minimum element in its place
+//		unsigned short temp = surround[tid][i];
+//		surround[tid][i] = surround[tid][minval];
+//		surround[tid][minval] = temp;
+//	}
+//
+//	// --- Pick the middle one
+//	Output_Image[(y * Image_Width) + x] = surround[tid][4];
+//
+//	__syncthreads();
+//
+//}
+
+__global__ void Kernel_Function_no_shared(unsigned char* Input_Image, unsigned char* Output_Image, int Image_Width, int Image_Height) {
+	unsigned short surround[9];
+
+	int iterator;
+
+	const int x = blockDim.x * blockIdx.x + threadIdx.x;
+	const int y = blockDim.y * blockIdx.y + threadIdx.y;
+	const int tid = threadIdx.y * blockDim.x + threadIdx.x;
+
+	if ((x >= (Image_Width - 1)) || (y >= Image_Height - 1) || (x == 0) || (y == 0)) return;
+
+	// --- Fill array private to the threads
+	iterator = 0;
+	for (int r = x - 1; r <= x + 1; r++) {
+		for (int c = y - 1; c <= y + 1; c++) {
+			surround[iterator] = Input_Image[c * Image_Width + r];
+			iterator++;
+		}
+	}
+
+	// --- Sort private array to find the median using Bubble Short
+	for (int i = 0; i < 5; ++i) {
+
+		// --- Find the position of the minimum element
+		int minval = i;
+		for (int l = i + 1; l < 9; ++l) if (surround[l] < surround[minval]) minval = l;
+
+		// --- Put found minimum element in its place
+		unsigned short temp = surround[i];
+		surround[i] = surround[minval];
+		surround[minval] = temp;
+	}
+
+	// --- Pick the middle one
+	Output_Image[(y * Image_Width) + x] = surround[4];
+}
+
+__global__ void resizeable_kernel(unsigned char* Input_Image, unsigned char* Output_Image, int Image_Width, int Image_Height) {
+	const int x = blockDim.x * blockIdx.x + threadIdx.x;
+	const int y = blockDim.y * blockIdx.y + threadIdx.y;
+	__shared__ unsigned short surround[BLOCK_WIDTH * BLOCK_HEIGHT][MEDIAN_LENGTH];
+	int iterator;
+	const int Haft_Of_MEDIAN_LENGTH = (MEDIAN_LENGTH) / 2 + 1;
+	int StartPoint = MEDIAN_DIMENSION / 2;
+	int EndPoint = StartPoint + 1;
+	
+	const int tid = threadIdx.y * blockDim.y + threadIdx.x;
+
+	if (x == 0 || x == Image_Width - StartPoint || y == 0 || y == Image_Height - StartPoint) {
+
+	}
+	else {
+		iterator = 0;
+		for (int r = x - StartPoint; r < x + EndPoint;r++) {
+			for (int c = y - StartPoint;c < y + (EndPoint); c++) {
+				surround[tid][iterator] = *(Input_Image + (c * Image_Width) + r);
+				iterator++;
+			}
+		}
+		for (int i = 0; i < Haft_Of_MEDIAN_LENGTH; ++i) {
+			int min = i;
+			for (int l = i + 1; l < MEDIAN_LENGTH; l++)
+				if (surround[tid][l] < surround[tid][min])
+					min = l;
+			unsigned char tmp = surround[tid][i];
+			surround[tid][i] = surround[tid][min];
+			surround[tid][min] = tmp;
+		}
+
+		*(Output_Image + (y * Image_Width) + x) = surround[tid][Haft_Of_MEDIAN_LENGTH - 1];
+		__syncthreads();
+	}
+}
+
+int main() {
+	//char image_path[] = "C:\\Users\\AustrianOak\\Pictures\\test_2.bmp";
+	//char output_path[] = "D:\\pythonProject\\Salt_Peper_Noise\\example.bmp";
+	// init bitmap image
+	Bitmap* originalImage = new Bitmap();
+	Bitmap* resultImageCPU = new Bitmap();
+	Bitmap* resultImageGPU = new Bitmap();
+	Bitmap* resultImageSharedGPU = new Bitmap();
+	originalImage->Load("D:\\Cppdev\\MedianFilter\\lena512.bmp");
+	//resultImageCPU->Load("D:\\Cppdev\\MedianFilter\\lena512.bmp");
+	resultImageGPU->Load("D:\\Cppdev\\MedianFilter\\lena512.bmp");
+	resultImageSharedGPU->Load("D:\\Cppdev\\MedianFilter\\lena512.bmp");
+	cudaError_t status;
+	
+	////////////////////////////////////////////////
+	int img_width = originalImage->Width();
+	int img_height = originalImage->Height();
+	
+	int size = img_width * img_height * sizeof(char);
+	unsigned char* deviceinputimage;
+	cudaMalloc((void**)&deviceinputimage, size);
+	status = cudaGetLastError();
+	if (status != cudaSuccess) {
+		std::cout << "Kernel failed for cudaMalloc : " << cudaGetErrorString(status) <<
+			std::endl;
+		return false;
+	}
+	cudaMemcpy(deviceinputimage, originalImage->image, size, cudaMemcpyHostToDevice);
+	status = cudaGetLastError();
+	if (status != cudaSuccess) {
+		std::cout << "Kernel failed for cudaMemcpy cudaMemcpyHostToDevice: " << cudaGetErrorString(status) <<
+			std::endl;
+		cudaFree(deviceinputimage);
+		return false;
+	}
+	unsigned char* deviceOutputImage;
+	cudaMalloc((void**)&deviceOutputImage, size);
+
+	///////////////////////////////////////////////
+	const dim3 grid(iDivUp(img_width, BLOCK_WIDTH), iDivUp(img_height, BLOCK_HEIGHT), 1);
+	const dim3 block(BLOCK_WIDTH, BLOCK_HEIGHT, 1);
+
+	float time;
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	
+	cudaEventRecord(start, 0);
+	//cudaFuncSetCacheConfig(Original_Kernel_Function, cudaFuncCachePreferShared);
+	//Original_Kernel_Function << <grid, block >> > (deviceinputimage, deviceOutputImage, img_width, img_height);
+	resizeable_kernel <<< grid, block >>> (deviceinputimage, deviceOutputImage, img_width, img_height);
+	cudaPeekAtLastError();
+	cudaDeviceSynchronize();
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&time, start, stop);
+	cout << "Original kernel function time:" << time << "ms" << endl;
+
+	cudaMemcpy(resultImageGPU->image, deviceOutputImage, size, cudaMemcpyDeviceToHost);
+	status = cudaGetLastError();
+
+	if (status != cudaSuccess) {
+		cout << "Kernel failed" << endl;
+		cudaFree(deviceinputimage);
+		cudaFree(deviceOutputImage);
+		return 1;
+	}
+	resultImageGPU->Save("D:\\pythonProject\\Salt_Peper_Noise\\example.bmp");
+	cudaFree(deviceinputimage);
+	cudaFree(deviceOutputImage);
+	return 0;
+	
+}
